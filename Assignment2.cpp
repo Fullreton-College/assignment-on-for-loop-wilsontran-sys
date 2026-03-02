ab01d8b31ea8fe0ef531caa953ce6225c06e2271
Tran, Wilson <wdtran2018@gmail.com>
2026-03-01 23:26:22 -0800

    Wrote a program that takes in two integers, and outputs the first integer and subsequent increments of 5 as long as the first value is less than the second

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
+   // Constants
+   const int INCREMENT = 5;
 
-   /* Type your code here. */
+   // Variables
+   int num1;
+   int num2;
+   
+   // Input
+   cout << "Enter the first integer: ";
+   cin >> num1;
+
+   cout << "Enter the second integer: ";
+   cin >> num2;
+
+   // Processing / output
+   if(num1 > num2)
+   {
+      cout << "\nSecond integer can't be less than the first." << endl;
+   }
+   else
+   {
+      cout << endl;
+      for(int i = num1; i <= num2; i += INCREMENT)
+      {
+         cout << i << " ";
+      }
+      cout << endl;
+   }
 
    return 0;
 }
